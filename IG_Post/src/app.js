847a61cbd3b55b7766349aef35a00cd5634f72ef
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-04 23:03:49 +0530

    `Added dependencies and updated package.json and app.js for image upload functionality`

--- IG_Post/src/app.js
+++ IG_Post/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 app.use(express.json());
+
+const upload = multer({storage: multer.memoryStorage()});
 
 app.get('/', (req, res) => {
     res.send('Welcome to the IG Post API');
 });
 
+app.post('/create-post', upload.single("image") , async (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+});
 
 
 module.exports = app; // Export the Express app instance
