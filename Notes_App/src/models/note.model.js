73ee65361a199d13b99156c3b316031a71246600
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-04 21:45:01 +0530

    Add note model with schema for title and description

---
+++ Notes_App/src/models/note.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const noteModel = mongoose.model('Note', noteSchema);
+
+module.exports = noteModel;
