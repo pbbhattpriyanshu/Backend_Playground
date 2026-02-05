20ef50f509665ead0817e86236d1f37e1febf35b
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-05 22:13:26 +0530

    `Added post model and updated create-post endpoint to store post data`

--- IG_Post/src/app.js
+++ IG_Post/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const uploadFile = require('./services/storage.service');
+const postModel = require('./models/post.model');
 
 const app = express();
 app.use(express.json());
@@ -13,7 +14,13 @@ app.get('/', (req, res) => {
 
 app.post('/create-post', upload.single("image") , async (req, res) => {
     const result = await uploadFile(req.file.buffer, req.file.originalname);
-    res.status(200).json({message: "Post created successfully", imageUrl: result.url});
+
+    const newPost = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+
+    res.status(200).json({message: "Post created successfully", post: newPost});
 });
 
 
