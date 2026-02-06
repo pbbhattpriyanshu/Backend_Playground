5f796e3dcecc44850dec8f931a5b5cb802b6b561
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-06 21:31:15 +0530

    `Added axios import and form submission handling to CreatePost component`

--- IG_Post/Frontend/src/Pages/CreatePost/CreatePost.jsx
+++ IG_Post/Frontend/src/Pages/CreatePost/CreatePost.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import axios from 'axios';
 import "./CreatePost.css";
 
 const CreatePost = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    axios.post("http://localhost:3000/create-post", formData)
+    .then((response) => {
+      alert("Post created successfully!");
+      console.log(response);
+      
+      e.target.reset();
+    }) .catch((error) => {
+      console.error("Error creating post:", error);
+      alert("Failed to create post. Please try again.");
+    });
+  }
   return (
     <div className="post-wrapper">
-      <form className="post-card">
+      <form className="post-card" onSubmit={handleSubmit}>
         <h2>Create Post</h2>
 
         <label className="upload-box">
