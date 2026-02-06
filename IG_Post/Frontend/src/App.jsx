5c4d218a05cf37f7258433d70e02f9f6918cd147
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-06 20:11:39 +0530

    `Added React Router configuration to App component`

--- IG_Post/Frontend/src/App.jsx
+++ IG_Post/Frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/create-post" element={<div>Create Post Page</div>} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
+      </Routes>
+    </Router>
   )
 }
 
