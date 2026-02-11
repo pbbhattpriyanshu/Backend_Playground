4a512ad72a025aa83c7bd808f86303cb0bf92be6
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-11 22:25:18 +0530

    feat: create user model with email, username, and password fields

---
+++ auth_lab/Backend/src/models/auth.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+        //TODO: Email format 
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
+
