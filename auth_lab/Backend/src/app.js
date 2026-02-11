85f54559ddca7f70d0bade14dd300c639bbced28
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-11 22:40:13 +0530

    feat: set up Express server with JSON parsing and authentication routes

--- auth_lab/Backend/src/app.js
+++ auth_lab/Backend/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const authRoute = require('./routes/auth.route');
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
+
+app.use('/api/auth', authRoute);
 
 module.exports = app;
