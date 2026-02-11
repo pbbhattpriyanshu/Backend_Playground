6ba79aa02b1a6644e7f358b55fc9704773b5c62e
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-11 22:21:01 +0530

    `Added dotenv and db connection configuration to Server.js and db.config.js`

--- auth_lab/Backend/Server.js
+++ auth_lab/Backend/Server.js
@@ -1,13 +1,15 @@
 const app = require('./src/app');
+require('dotenv').config();
+const connectDB = require('./src/config/db.config');
 
+const PORT = process.env.PORT || 5000;
 
-
-
+connectDB();
 
 
 
 
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
 })
