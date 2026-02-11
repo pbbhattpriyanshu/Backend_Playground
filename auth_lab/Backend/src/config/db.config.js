52e9709b32f9ab8e25e1760f301583a593be0aee
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-11 22:17:47 +0530

    feat: add MongoDB connection configuration using mongoose

---
+++ auth_lab/Backend/src/config/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+function connect() {
+  mongoose.connect(process.env.MONGO_URI).then( () => {
+    console.log('Connected to MongoDB');
+  }).catch(err => {
+    console.error('MongoDB connection error:', err.message);
+  });
+}
+
+module.exports = connect;
