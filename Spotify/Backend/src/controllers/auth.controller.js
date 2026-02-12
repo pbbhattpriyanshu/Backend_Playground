0c627942560191b354fbf8aeb7dc88c3fc2b700b
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-12 20:27:39 +0530

    Add signup controller with email and password validation

---
+++ Spotify/Backend/src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+const userModel = require("../models/auth.model");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+const signup = async (req, res) => {
+  // Extract email, username, and password from the request body
+  const { email, username, password } = req.body;
+
+  // Email validation
+  const isEmailValid = validator.isEmail(email);
+
+  if (!isEmailValid) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  // Password validation
+  function isStrongPassword(password) {
+    return validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    });
+  }
+
+  if (!isStrongPassword(password)) {
+    return res.status(400).json({
+      message:
+        "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one symbol.",
+    });
+  }
+
+  try {
+    const isUserExist = await userModel.findOne({ email });
+
+    if (isUserExist) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const user = await userModel.create({ email, username, password });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.cookie("token", token);
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
+  }
+};
+
+module.exports = { signup };
