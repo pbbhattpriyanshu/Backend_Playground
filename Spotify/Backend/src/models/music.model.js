fe1792b7d7dd28b68fa04a4328fb4144f7628a82
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-12 23:07:38 +0530

    Add music model with schema for music entries

---
+++ Spotify/Backend/src/models/music.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const musicSchema = new mongoose.Schema({
+    uri: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    info: {
+        type: Object
+    }
+})
+
+const Music = mongoose.model('music', musicSchema);
+module.exports = Music;
