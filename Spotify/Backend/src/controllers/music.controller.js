57ee8063c2c44e18385f270749ef799662fcc14b
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-14 19:10:58 +0530

    feat: Implement album model and controller functions for creating music and albums.

--- Spotify/Backend/src/controllers/music.controller.js
+++ Spotify/Backend/src/controllers/music.controller.js
@@ -1,7 +1,9 @@
 const Music = require("../models/music.model");
 const uploadFile = require("../services/storage.service");
+const Album = require("../models/album.model");
 const jwt = require("jsonwebtoken");
 
+// Create music
 const createMusic = async (req, res) => {
   const token = req.cookies.token;
 
@@ -40,5 +42,38 @@ const createMusic = async (req, res) => {
   }
 };
 
+// Create album
+const createAlbum = async (req, res) => {
+  const token = req.cookies.token;
 
-module.exports = { createMusic };
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "artist") { return res.status(403).json({ message: "You don't have an access to create an album" }); }
+
+    const { title, artist, musics } = req.body;
+
+    const album = await Album.create({
+      title,
+      musics,
+      artist: decoded.id
+    });
+
+    res.status(201).json({ message: "Album created successfully", album: {
+      id: album._id,
+      title: album.title,
+      artist: album.artist,
+      musics: album.musics
+    } });
+
+  } catch (error) {
+    console.log("Error creating album:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { createMusic, createAlbum };
