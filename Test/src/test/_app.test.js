274b1c72f172c7c8821bd1e02f9c260065eda7f1
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-15 00:00:11 +0530

    chore: Update project dependencies by adding and modifying various `node_modules` files.

---
+++ Test/src/test/_app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('App', () => {
+    it('should return 200 OK', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({
+            message: 'Hello World!'
+        });
+    });
+}); 
