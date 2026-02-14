86b0e299c00c8b99e3fd57e206b5c7349f9d561e
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-14 17:50:32 +0530

    feat: implement ImageKit file upload service for handling file uploads.

--- Spotify/Backend/src/services/storage.service.js
+++ Spotify/Backend/src/services/storage.service.js
@@ -7,11 +7,12 @@ const imagekit  = new ImageKit({
 })
 
 // Function to upload a file
-async function uploadFile(buffer, filename) {
+async function uploadFile(file  ) {
     try {
         const result = await imagekit.files.upload({
-            file: buffer.toString('base64'), // Convert buffer to base64 string
-            fileName: filename,
+            file,
+            fileName: "music_" + Date.now(),
+            folder: "music",
         });
         
         return result;
